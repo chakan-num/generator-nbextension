ab083638c9df29691f6bfd44f82bf69c38fa8309
Marcos Vanetta <marcosvanetta@gmail.com>
2016-03-11 15:03:53 -0700

    New post-link and pre-unlink files

--- generators/app/templates/conda.recipe/post-link.bat
+++ generators/app/templates/conda.recipe/post-link.bat
@@ -1,1 +1,4 @@
-"%PREFIX%/bin/python" -m <%= parameterized %>.setup install --enable --prefix="%PREFIX%"
+"%PREFIX%\Scripts\jupyter" nbextension install --py="%PKG_NAME%" --overwrite --sys-prefix
+"%PREFIX%\Scripts\jupyter" nbextension enable --py="%PKG_NAME%" --sys-prefix
+"%PREFIX%\Scripts\jupyter" serverextension enable --py="%PKG_NAME%" --sys-prefix
+errorlevel 1 exit 1
